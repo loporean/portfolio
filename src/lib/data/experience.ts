95eef1fd0125ea33fcd8d8178ef0600cba4bbb4b
loporean <84533280+loporean@users.noreply.github.com>
2024-08-01 21:54:20 -0700

    remove default data

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -5,20 +5,20 @@ import { ContractType, type Experience } from '../types';
 export const items: Array<Experience> = [
 	
 	{
-		slug: '',
-		company: '',
-		description: '',
+		slug: 'open-sourcer',
+		company: 'Self-employed',
+		description: 'Creating awesome tools for developers.',
 		contract: ContractType.SelfEmployed,
-		type: '',
+		type: 'Software Development',
 		location: 'Home',
 		period: { from: new Date() },
 		skills: getSkills(),
-		name: '',
+		name: 'Open Source Developer',
 		color: 'red',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: ''
-	}
+		shortDescription: 'Creating awesome tools for developers.'
+	},
 	// {
 	// 	slug: 'open-sourcer',
 	// 	company: 'Self-employed',
